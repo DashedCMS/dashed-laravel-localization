409acd51b074266c6c40ce0fc406b3c656e4cfc9
Robin van Maasakker <robin@qubiqx.com>
2024-11-20 12:56:32 +0100

    first commit

---
+++ src/Dashed/LaravelLocalization/Exceptions/UnsupportedLocaleException.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Dashed\LaravelLocalization\Exceptions;
+
+use Exception;
+
+class UnsupportedLocaleException extends Exception
+{
+}
---
+++ tests/ModelWithTranslatableRoutes.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Eloquent\Model;
+use Dashed\LaravelLocalization\Interfaces\LocalizedUrlRoutable;
+
+class ModelWithTranslatableRoutes extends Model implements LocalizedUrlRoutable
+{
+    public function getLocalizedRouteKey($locale)
+    {
+        if($locale == 'es'){
+            return 'empresa';
+        }
+
+        return 'company';
+    }
+}
